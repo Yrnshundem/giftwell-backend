0eb1bb5ae86352fcc04ff28be2979131e0cf5523
Yrnshundem <anitaleele53@gmail.com>
2025-04-17 12:52:51 +0000

    Update order.js

--- models/order.js
+++ models/order.js
@@ -1,24 +1,24 @@
 const mongoose = require("mongoose");
 
 const itemSchema = new mongoose.Schema({
-    productId: { type: String, required: true },
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    quantity: { type: Number, required: true, min: 1 }
+  productId: { type: String, required: true },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true, min: 1 },
 });
 
 const orderSchema = new mongoose.Schema({
-    userId: { type: String, required: false }, // String to support "guest"
-    fullName: { type: String, required: true },
-    phone: { type: String, required: true },
-    address: { type: String, required: true },
-    city: { type: String, required: true },
-    country: { type: String, required: true },
-    items: { type: [itemSchema], required: true },
-    total: { type: Number, required: true },
-    status: { type: String, default: "pending" },
-    paymentMethod: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now }
+  userId: { type: String, required: false }, // String to support "guest"
+  fullName: { type: String, required: true },
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+  items: { type: [itemSchema], required: true },
+  total: { type: Number, required: true },
+  status: { type: String, default: "pending" },
+  paymentMethod: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
 });
 
 // Prevent OverwriteModelError

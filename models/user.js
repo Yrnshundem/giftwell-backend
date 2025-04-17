0da9aad4e41157ac4200c9652505850cb8cb250e
Yrnshundem <anitaleele53@gmail.com>
2025-04-17 13:11:51 +0000

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    fullName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, default: "user" } // Default role is 'user'
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, default: "user" },
 });
 
 module.exports = mongoose.model("User", UserSchema);
-
-

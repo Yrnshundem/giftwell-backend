f7d408a68ebc51ed52a5d0ed062e728c4b6cb5c1
Yrnshundem <anitaleele53@gmail.com>
2025-04-17 12:14:59 +0000

    Update cart.js

--- models/cart.js
+++ models/cart.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
 
-const itemSchema = new mongoose.Schema({
-    productId: { type: String, required: true }, // String since products array uses string IDs
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    image: { type: String, required: true },
-    quantity: { type: Number, default: 1, min: 1 }
-});
-
 const cartSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
-    items: [itemSchema],
-    total: { type: Number, default: 0 }
+  userId: { type: String, required: true },
+  items: [
+    {
+      productId: { type: String, required: true },
+      name: { type: String, required: true },
+      price: { type: Number, required: true },
+      quantity: { type: Number, required: true, default: 1 },
+    },
+  ],
+  total: { type: Number, default: 0 },
 });
 
-module.exports = mongoose.model("cart", cartSchema);
+module.exports = mongoose.model("Cart", cartSchema);

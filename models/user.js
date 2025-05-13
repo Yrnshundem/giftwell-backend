ad3b7913d2345d36e9da33a6dd6a7d605524b65f
Yrnshundem <anitaleele53@gmail.com>
2025-05-13 22:22:34 +0000

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs"); // Use bcryptjs for consistency
 
 const userSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
   role: { type: String, default: "user" }
+});
+
+// Pre-save hook to hash password
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);

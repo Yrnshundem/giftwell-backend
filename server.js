b2bf15c36a5816967b7629ea4b0e2bf2ce7cb3e1
Yrnshundem <anitaleele53@gmail.com>
2025-05-14 19:47:25 +0000

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(
         origin: [
             "https://gift-well-frontend.vercel.app",
             "https://giftwell.pro",
+            "https://www.giftwell.pro", 
             "http://localhost:3000"
         ],
         credentials: true,
